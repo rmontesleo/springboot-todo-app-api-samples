ca29427a18ee49900a68663811fdb55f7c3328fd
Leopoldo Ramirez Montes <rmontesleo@gmail.com>
2024-10-23 23:20:44 -0600

    add more samples

---
+++ springboot-todo-h2-api-iaas-main/src/main/java/com/demo/todoapi/entities/User.java
@@ -0,0 +1,65 @@
+package com.demo.todoapi.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Table( name="users")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Long userId;
+
+    @Column(name ="user_name")
+    private String userName;
+
+    @JsonIgnore
+    private String password;
+
+
+    @OneToMany( mappedBy = "user", cascade = CascadeType.ALL )
+    @ToString.Exclude
+    @JsonIgnore
+    private Set<Todo> todos;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return userId != null && Objects.equals(userId, user.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
---
+++ springboot-todo-postgres-api-main/src/main/java/com/demo/todoapi/entities/Todo.java
@@ -0,0 +1,76 @@
+package com.demo.todoapi.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import org.hibernate.Hibernate;
+
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table( name ="todos")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Todo {
+
+    @Id
+    @Column( name = "id" )
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long todoId;
+
+    @Column( name="created_by")
+    private Long createdBy;
+
+    @Column( name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column( name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    private String title;
+
+    private String description;
+
+    @Column(name = "is_completed")
+    private Boolean completed;
+
+
+    @ManyToOne( fetch = FetchType.LAZY )
+    @JoinColumn(name = "created_by", insertable = false, updatable = false )
+    @ToString.Exclude
+    @JsonIgnore
+    private User user;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Todo todo = (Todo) o;
+        return todoId != null && Objects.equals(todoId, todo.todoId);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
